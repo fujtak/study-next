6a54147a5525f3e4a1d340ded417bf3736444979
fujtak <fuj.eng23@gmail.com>
2025-09-07 19:06:53 +0900

    create-next-app

--- pages/api/hello.ts
+++
@@ -1,7 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next"
-
-const hello = (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json({ message: 'hello' })
-}
-
-export default hello
--- pages/static.tsx
+++
@@ -1,9 +0,0 @@
-import { NextPage } from 'next'
-
-const PageStatic: NextPage = () => {
-  return (
-    <h1>staticページ</h1>
-  )
-}
-
-export default PageStatic
