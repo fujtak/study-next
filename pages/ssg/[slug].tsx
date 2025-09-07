d04baf032e2053cdd064889223beb1c5f4b7c6f7
fujtak <fuj.eng23@gmail.com>
2025-09-07 15:03:51 +0900

    複数のSSGページを作成（fallback: false→パス外のアクセスに対し404自動表示）

--- pages/ssg/[slug].tsx
+++ pages/ssg/[slug].tsx
@@ -1,11 +1,37 @@
-import { NextPage } from "next"
+import { NextPage, GetStaticPaths ,GetStaticProps } from "next"
 
-const PageStaticMulti: NextPage = () => {
+type Props = {
+  message: string
+}
+
+const PageSSGMulti: NextPage<Props> = (props) => {
+  const { message } = props
   return (
     <>
-      <h1>複数のStaticページ</h1>
+      <h1>複数のSSGページ</h1>
+      <p>{ message }</p>
     </>
   )
 }
 
-export default PageStaticMulti
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [
+      { params: { slug: '1' } },
+    ],
+    fallback: false
+  }
+}
+
+export const getStaticProps: GetStaticProps = (context) => {
+  const message = context.params?.slug
+    ? `【slug:${context.params?.slug}】SSGビルド時にpropsに渡されたメッセージ`
+    : ''
+  return {
+    props: {
+      message
+    }
+  }
+}
+
+export default PageSSGMulti
