213e901064a7efbbb7ce1b9092c9271e7087243e
fujtak <fuj.eng23@gmail.com>
2025-09-15 20:18:00 +0900

    jestを導入

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,23 @@
 import type { NextConfig } from "next";
 
+const compiler = () => {
+  const config = {
+    styledComponents: true,
+  }
+  if(process.env.NODE_ENV === 'production') {
+    const configProd = {
+      // 本番環境ではReact Testing Libraryで使用するdata-testid属性を削除
+      reactRemoveProperties: { properties: ['^data-custom$'] },
+    }
+    Object.assign(config, configProd)
+  }
+  return config
+}
+
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  compiler: {
-    styledComponents: true,
-  },
+  compiler: compiler(),
 };
 
 export default nextConfig;
