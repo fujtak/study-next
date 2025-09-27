84252b9905b70385eec1666b51ab344c5319d877
fujtak <fuj.eng23@gmail.com>
2025-09-27 20:05:13 +0900

    init: 🎉 TypeScriptを設定（Next.js同梱）

---
+++ src/app/layout.tsx
@@ -0,0 +1,11 @@
+type LayoutProps = {
+  children: React.ReactNode
+}
+
+export default function RootLayout({ children }: LayoutProps) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
